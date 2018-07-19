ac0fbd78edf67650be700458b8c81df8f01de4c4
Ondrey Mischenko <showerdownpour@gmail.com>
2018-07-19 23:12:37 +0300

    Made get methods for Link

--- app/src/main/java/com/rdc/androidtestappa/Link.java
+++ app/src/main/java/com/rdc/androidtestappa/Link.java
@@ -1,19 +1,30 @@
 package com.rdc.androidtestappa;
 
-import android.icu.util.LocaleData;
+import java.util.Date;
 
-import java.time.LocalDate;
 
 public class Link {
     private int id;
     private String url;
     private int status;
-    private LocalDate date;
+    private Date date;
+
     Link(int id,String url,int status){
         this.id = id;
         this.url = url;
         this.status = status;
-        //add a variable date
-      //  LocalDate date = new LocalDate();
+        this.date = new Date();
+    }
+
+    int getIdLink() {
+        return id;
+    }
+
+    String getUrlLink() {
+        return url;
+    }
+
+    int getStatusLink() {
+        return status;
     }
 }
