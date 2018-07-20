4aaada474f89fd45f8ffd7e6f623970594b97800
Dan <denis_prozor@bk.ru>
2018-07-20 13:31:46 +0300

    Second tab change

--- app/src/main/java/com/rdc/androidtestappa/SecondTab.java
+++ app/src/main/java/com/rdc/androidtestappa/SecondTab.java
@@ -1,7 +1,21 @@
 package com.rdc.androidtestappa;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondTab extends Fragment{
-    
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.second_tab_layout, container, false);
+            return rootView;
+        }
+
 }
