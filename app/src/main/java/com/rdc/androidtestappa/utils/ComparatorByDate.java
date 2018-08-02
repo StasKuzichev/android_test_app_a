85d4b147ebcca7102a01792dd282b2ace60271c8
Anna Shipilova <anyashi2002@gmail.com>
2018-08-02 15:26:18 +0300

    Fixed a problem with date sorting

--- app/src/main/java/com/rdc/androidtestappa/utils/ComparatorByDate.java
+++ app/src/main/java/com/rdc/androidtestappa/utils/ComparatorByDate.java
@@ -2,11 +2,22 @@ package com.rdc.androidtestappa.utils;
 
 import com.rdc.androidtestappa.Link;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Comparator;
+import java.util.Date;
 
 public class ComparatorByDate implements Comparator<Link> {
     @Override
     public int compare(Link linkFirst , Link linkSecond) {
-        return linkFirst.getDate().compareTo(linkSecond.getDate());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yy HH:mm");
+        try {
+            Date dateOne = simpleDateFormat.parse(linkFirst.getDate());
+            Date dateTwo = simpleDateFormat.parse(linkSecond.getDate());
+            return dateTwo.compareTo(dateOne);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 }
